26c4fa119e824ef13dfc9d0d65a9e28f66a68467
Walegi4 <129233042+WalmartBagGuy@users.noreply.github.com>
2023-04-21 18:47:08 -0500

    Step 37
    It's Step 37

--- RAD- Booper Shapes/Assets/Scripts/GameController.cs
+++ RAD- Booper Shapes/Assets/Scripts/GameController.cs
@@ -5,10 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+
+    //Hexagon game object
+    [Header("Shape Objects")]
+    public GameObject[] shapePrefabs;
+    //Spawn rate for how many
+    //objects are spawned
+    [Header("Default Spawn Delay Time")]
+    public float spawnDelay = 2f;
+    [Header("Default Spawn Time")]
+    public float spawnTime = 3f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        InvokeRepeating("Spawn", spawnDelay, spawnTime);
     }
 
     // Update is called once per frame
@@ -16,4 +27,21 @@ public class GameController : MonoBehaviour
     {
 
     }
+
+    //Spawn function
+    void Spawn()
+    {
+        //Random range for shape objects to be spawned
+        int randomInt = Random.Range(0, shapePrefabs.Length);
+        //Spawn new hexagon at position
+        Instantiate(shapePrefabs[randomInt], Vector3.zero, Quaternion.identity);
+    }
+
+    //Game over function
+    public void GameOver()
+    {
+        //Stops the spawn function
+        CancelInvoke("Spawn");
+    }
+
 }
